5e0e98ba732b53ac2f41651e363b3c3ef529c2a8
Brian Lee Yung Rowe <rowe@nurometic.com>
2011-12-23 11:25:11 -0500

    Revert "Fixed issue with new API for amqp_connection:start()"
    This reverts commit d78387c968354319f140ae3ab3474294dcac1a9c.

--- src/bunny_farm.erl
+++ src/bunny_farm.erl
@@ -195,15 +195,10 @@ open_it(#bus_handle{}=BusHandle) ->
   [U,P,V] = lists:map(fun get_env/1, Keys),
   Params = #amqp_params_network{username=U, password=P, virtual_host=V,
              host=H, port=R},
-  open_it(Params, BusHandle).
+  open_it(network, Params, BusHandle).
 
-open_it(#amqp_params_network{}=Params, #bus_handle{}=BusHandle) ->
-  {ok,Connection} = amqp_connection:start(Params),
-  {ok,Channel} = amqp_connection:open_channel(Connection),
-  BusHandle#bus_handle{channel=Channel, conn=Connection};
-
-open_it(#amqp_params_direct{}=Params, #bus_handle{}=BusHandle) ->
-  {ok,Connection} = amqp_connection:start(Params),
+open_it(Method, #amqp_params_network{}=Params, #bus_handle{}=BusHandle) ->
+  {ok,Connection} = amqp_connection:start(Method, Params),
   {ok,Channel} = amqp_connection:open_channel(Connection),
   BusHandle#bus_handle{channel=Channel, conn=Connection}.
 
